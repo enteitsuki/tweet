56af5de28e95115d8ddfd5c84d8db7dd305369f2
enteitsuki <enteitsuki@gmail.com>
2020-11-19 22:34:36 +0900

    編集画面 お気に入り機能追加

--- show.php
+++ show.php
@@ -24,6 +24,7 @@ if (!$tweet) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tweet</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1><?= h($tweet['content']) ?></h1>
@@ -32,6 +33,11 @@ if (!$tweet) {
         <li>
             [#<?= h($tweet['id']) ?>]<?= h($tweet['content']) ?><br>
             投稿日時: <?= h($tweet['created_at']) ?>
+            <?php if ($tweet['good'] == false) : ?>
+                <a href="good.php?id=<?= h($tweet['id']) ?>" class="good-link">☆</a>
+            <?php else : ?>
+                <a href="good.php?id=<?= h($tweet['id']) ?>" class="bad-link">★</a>
+            <?php endif; ?>
             <a href="edit.php?id=<?= h($tweet['id']) ?>">[編集]</a>
             <a href="delete.php?id=<?= h($tweet['id']) ?>">[削除]</a>
             <hr>
